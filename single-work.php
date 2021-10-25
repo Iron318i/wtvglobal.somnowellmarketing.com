3e30b23954796720474c4118a07508ebae82fd4e
shilkomariya <shilkomariya@gmail.com>
2021-10-25 19:29:21 +0300

    19-20

--- single-work.php
+++ single-work.php
@@ -22,6 +22,30 @@ get_header();
     }
     ?>
 </div>
+<section class="bg-gray">
+    <div class="container py-3 w-1440">
+	<div class="fw-heading fw-heading-h3 text-center lines mb-3"><h3 class="fw-special-title fw-normal">Other case studies</h3></div>
+	<div class="row cards-row">
+	    <?php
+	    $args = array(
+		'posts_per_page' => 4,
+		'post_type' => 'work',
+		'post__not_in' => array(get_the_ID())
+	    );
+
+	    $query = new WP_Query($args);
+
+	    if ($query->have_posts()) {
+		while ($query->have_posts()) {
+		    $query->the_post();
+		    get_template_part('loop-templates/content');
+		}
+	    }
+	    wp_reset_postdata();
+	    ?>
+	</div>
+    </div>
+</section>
 <?php
 get_template_part('template-parts/start-conversation');
 get_footer();
