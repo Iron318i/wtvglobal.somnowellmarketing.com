1cb48c6ec3bea02806fad7718a54ec2f4545fc71
shilkomariya <shilkomariya@gmail.com>
2021-10-27 16:53:01 +0300

    16-50

--- framework-customizations/extensions/shortcodes/shortcodes/we-deliver/views/view.php
+++ framework-customizations/extensions/shortcodes/shortcodes/we-deliver/views/view.php
@@ -6,10 +6,12 @@ if (!defined('FW')) {
 <div class="row cards-row we-deliver">
     <?php
     $arr = $atts['cards'];
+    $numItems = count($arr);
+    $i = 0;
     foreach ($arr as $key => $value) {
 	?>
         <div class="col">
-    	<div class="card">
+    	<div class="card <?php if (++$i === $numItems) echo " last"; ?>">
     	    <div class="card-img-top">
 		    <?php echo wp_get_attachment_image($value['image']["attachment_id"], 'thumbnail', false, array("class" => "normal")); ?>
 		    <?php echo wp_get_attachment_image($value['image_invert']["attachment_id"], 'thumbnail', false, array("class" => "hover")); ?>
@@ -18,9 +20,10 @@ if (!defined('FW')) {
     		<h5 class="card-title"><?php echo $value['heading']; ?></h5>
     		<div class="card-text"><?php echo $value['descriprion']; ?></div>
     	    </div>
+		<?php if ($i === $numItems) { ?>
+		    <a href="#contact-form" class="stretched-link"></a>
+		<?php } ?>
     	</div>
         </div>
-	<?php
-    }
-    ?>
+    <?php } ?>
 </div>
