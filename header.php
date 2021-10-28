9086f31e124c51edbb9c5865d89a78791a587195
shilkomariya <shilkomariya@gmail.com>
2021-10-28 17:15:13 +0300

    17-15

--- header.php
+++ header.php
@@ -31,13 +31,46 @@ defined('ABSPATH') || exit;
 	<link rel="preconnect" href="https://fonts.googleapis.com">
 	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 	<?php wp_head(); ?>
+	<script src="https://www.google.com/recaptcha/api.js"></script>
+	<script>
+	    function timestamp() {
+		var response = document.getElementById("g-recaptcha-response");
+		if (response == null || response.value.trim() == "") {
+		    var elems = JSON.parse(document.getElementsByName("captcha_settings")[0].value);
+		    elems["ts"] = JSON.stringify(new Date().getTime());
+		    document.getElementsByName("captcha_settings")[0].value = JSON.stringify(elems);
+		}
+	    }
+	    setInterval(timestamp, 500);
+	</script>
+
+
+	<script>
+	    (function () {
+
+		window.ldfdr = window.ldfdr || {};
+		(function (d, s, ss, fs) {
+		    fs = d.getElementsByTagName(s)[0];
+
+		    function ce(src) {
+			var cs = d.createElement(s);
+			cs.src = src;
+			setTimeout(function () {
+			    fs.parentNode.insertBefore(cs, fs)
+			}, 1);
+		    }
+
+		    ce(ss);
+		})(document, 'script', 'https://sc.lfeeder.com/lftracker_v1_JMvZ8gk96Md82pOd.js');
+	    })();
+	</script>
     </head>
     <body <?php body_class(); ?> <?php wtvglobal_body_attributes(); ?>>
 	<!-- Google Tag Manager (noscript) -->
 	<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-KGVCKMK"
 			  height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
 	<!-- End Google Tag Manager (noscript) -->
-	<?php do_action('wp_body_open'); ?>
+			<?php do_action('wp_body_open'); ?>
 	<header class="site-header">
 	    <nav class="navbar navbar-expand-lg navbar-dark">
 		<div class="container w-1440">
